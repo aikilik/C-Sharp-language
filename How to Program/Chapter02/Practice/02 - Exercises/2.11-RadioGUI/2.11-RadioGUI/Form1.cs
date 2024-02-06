52af67c8ac8e540a699adee88c5f1bd4c33f79fe
aikilik <thallonys@gmail.com>
2024-02-06 09:39:34 -0300

    C#

---
+++ How to Program/Chapter02/Practice/02 - Exercises/2.11-RadioGUI/2.11-RadioGUI/Form1.cs
@@ -0,0 +1,25 @@
+namespace _2._11_RadioGUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
